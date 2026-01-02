d89ea598f776de151759a5307c9f62d91c269079
Domenic Pittari <dom.pittari@gmail.com>
2026-01-02 10:20:11 +0900

    feat: initialize project structure with TanStack Router and SSR integration

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import tailwindcss from "@tailwindcss/vite"
+import { devtools } from "@tanstack/devtools-vite"
+import { tanstackStart } from "@tanstack/react-start/plugin/vite"
+import viteReact from "@vitejs/plugin-react"
+import { nitro } from "nitro/vite"
+import viteTsConfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
+
+const config = defineConfig({
+	plugins: [
+		devtools(),
+		nitro(),
+		viteTsConfigPaths({
+			projects: ["./tsconfig.json"],
+		}),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+	],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		css: true,
+	},
+})
+
+export default config
